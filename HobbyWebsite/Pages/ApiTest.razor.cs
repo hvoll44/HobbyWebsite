36cdf16329137368c160ccfc0d379665cdd9387d
Hans Voll <hvoll44@gmail.com>
2021-12-20 22:05:55 -0500

    add random fact api

---
+++ HobbyWebsite/Pages/ApiTest.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace HobbyWebsite.Pages
+{
+    public partial class ApiTest
+    {
+        private RandomCatFact _json = new RandomCatFact();
+
+        protected override async Task OnInitializedAsync()
+        {
+            _json = await Http.GetFromJsonAsync<RandomCatFact>("https://catfact.ninja/fact");
+        }
+    }
+
+    public class RandomCatFact
+    {
+        public string Fact { get; set; }
+
+        public int Length { get; set; }
+    }
+}
